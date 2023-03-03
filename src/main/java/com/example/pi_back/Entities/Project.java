7b1d347f5d8e9339b035a0f521f7705bf8c7ee30
Phenix0103 <cyrine.chouchane@esprit.tn>
2023-03-03 01:53:11 +0100

    cyrine

---
+++ src/main/java/com/example/pi_back/Entities/Project.java
@@ -0,0 +1,32 @@
+package com.example.pi_back.Entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Project implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String estimated_time;
+    private float investable_amout;
+    private int numberInv;
+    private Double shareInv;
+
+
+
+    @ManyToMany
+    private Set<User> users;
+
+
+}
---
+++ src/main/java/com/example/pi_back/Repositories/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.example.pi_back.Repositories;
+
+import com.example.pi_back.Entities.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+}
